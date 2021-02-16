0428c6c5683fdc8526e52a8bd90670fef1edbabc
Pawan Chandra Joshi <48177844+pawan023@users.noreply.github.com>
2021-02-16 12:27:51 +0530

    Update 10 count inversions in an array.cpp

--- Searching & Sorting/10 count inversions in an array.cpp
+++ Searching & Sorting/10 count inversions in an array.cpp
@@ -2,7 +2,7 @@
 // count inversions in an array
 //------------------------------------------------------------------------------------
 /*
-Inversion Count for an array indicates – how far (or close) the array is from being sorted.
+Inversion Count for an array indicates â€“ how far (or close) the array is from being sorted.
 If array is already sorted then inversion count is 0. If array is sorted in reverse order 
 that inversion count is the maximum. 
 Formally speaking, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j 
@@ -82,5 +82,4 @@ int main(){
 	for(int i=0; i<5; i++) cout<<arr[i]<<" ";
 
 }
-
 
