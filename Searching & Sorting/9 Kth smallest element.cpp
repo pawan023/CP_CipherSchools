f4eef004e89011193954e63f4a5b75c91ad09ebd
Pawan Chandra Joshi <48177844+pawan023@users.noreply.github.com>
2021-02-16 12:04:37 +0530

    Update 9 Kth smallest element.cpp

--- Searching & Sorting/9 Kth smallest element.cpp
+++ Searching & Sorting/9 Kth smallest element.cpp
@@ -1,9 +1,9 @@
 //------------------------------------------------------------------------------------
-// K’th Smallest Element in Unsorted Array
+// Kâ€™th Smallest Element in Unsorted Array
 //------------------------------------------------------------------------------------
 /*
 Given an array and a number k where k is smaller than size of array, 
-we need to find the k’th smallest element in the given array. It is given that all array elements are distinct.
+we need to find the kâ€™th smallest element in the given array. It is given that all array elements are distinct.
 
 Input: arr[] = {7, 10, 4, 3, 20, 15} 
 k = 3 
@@ -61,5 +61,4 @@ int main(){
 	int ans = ind<0 ? -1 : arr[ind];
 	cout<<ans;
 }
-
 
