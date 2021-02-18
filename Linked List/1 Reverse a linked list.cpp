b619d0a35eb74ab4e660bbb9caee128a1cb24fbd
Pawan Chandra Joshi <48177844+pawan023@users.noreply.github.com>
2021-02-18 21:33:05 +0530

    Create 1 Reverse a linked list.cpp

---
+++ Linked List/1 Reverse a linked list.cpp
@@ -0,0 +1,47 @@
+// Reverse a linked list
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+//1
+struct Node* reverse(struct Node *curr, struct Node *prev){
+    if(curr==NULL) return prev;
+    if(curr->next==NULL) return curr;
+    Node *next = curr->next;
+    curr->next = prev;
+    return reverse(curr,next);
+}
+struct Node* reverseList(struct Node *head)
+{
+    Node *prev=NULL, *curr=head;
+    return reverse(curr,prev);
+}
+
+//2
+struct Node* reverseList(struct Node *head)
+{
+    if(head==NULL or head->next==NULL) return head;
+    
+    Node *rest_head = reverseList(head->next);
+    Node *rest_tail = head->next;
+    rest_tail->next = head;
+    head->next = NULL;
+    
+    return rest_head;
+}
+
+//3
+struct Node* reverseList(struct Node *head)
+{
+    Node *prev=NULL, *curr=head;
+    while(curr){
+        Node *next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
