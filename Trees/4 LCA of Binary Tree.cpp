10ba5973ac2cc683bcb512908506ed5b416e004e
Pawan Chandra Joshi <48177844+pawan023@users.noreply.github.com>
2021-02-20 21:14:34 +0530

    Create 4 LCA of Binary Tree.cpp

---
+++ Trees/4 LCA of Binary Tree.cpp
@@ -0,0 +1,16 @@
+// LCA of Binary Tree
+
+Node* lca(Node* root ,int n1 ,int n2 ){
+    
+    if(root==NULL) return NULL;
+    if(root->data==n1 or root->data==n2) return root;
+    
+    Node *lca1 = lca(root->left,n1,n2);
+    Node *lca2 = lca(root->right,n1,n2);
+    
+    if(lca1!=NULL && lca2!=NULL) return root;
+    if(lca1!=NULL) return lca1;
+    else return lca2;
+
+}
+
